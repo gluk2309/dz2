424f6bee3e3e8874d071d0a49b436a8da8c685d2
gluk2309 <gluk2309@mail.ru>
2023-07-24 12:29:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Your miles");
+
+        int ticket = 12_000;
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+
+        System.out.println(miles);
+
+
+
+    }
+}
